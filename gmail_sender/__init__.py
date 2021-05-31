1d44b759a6450113bde75e41ce4e77c935b6ec2a
Pydevoleper <85053735+Pydevoleper@users.noreply.github.com>
2021-05-31 12:16:34 +0300

    Update __init__.py

--- gmail_sender/__init__.py
+++ gmail_sender/__init__.py
@@ -1,1 +1,5 @@
-from gmail_sender import *
+
+from gmail_sender import send_html_gmail_to_one
+from gmail_sender import send_gmail_to_one
+from gmail_sender import send_gmail_to_people_in_list
+from gmail_sender import send_html_gmail_to_people_in_list
