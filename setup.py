54373b492eb77417635be60fbf7204a24230aa37
Pydevoleper <85053735+Pydevoleper@users.noreply.github.com>
2021-06-16 19:12:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     LONG_DESCRIPTION  = f.read()
     
-VERSION = '0.1.1'
+VERSION = '0.1.2'
 DESCRIPTION = 'gmail sending lib'
 URL = 'https://pypi.org/project/mail-sending-program/'
 # Setting up
@@ -14,19 +14,21 @@ setup(
     name="mail sending program",
     version=VERSION,
     author="py devoleper",
-    author_email="<mailsender.help@gmail.com>",
+    author_email="<mail.program.help@gmail.com>",
     url=URL,
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    keywords=['python', 'gmail', 'html', 'stmlp'],
+    keywords=['python', 'gmail', 'html', 'stmlp', 'auto', ''],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
         "Programming Language :: Python :: 3",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
+        "Development Status :: 4 - Beta",
+        "Operating System :: POSIX :: Linux",
     ],
     license='MIT'
 )
