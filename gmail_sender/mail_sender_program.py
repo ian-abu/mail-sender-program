1077a9877d377e1d96eb63c36c300da316abf633
Pydevoleper <85053735+Pydevoleper@users.noreply.github.com>
2021-06-19 17:47:34 +0300

    Add files via upload

--- gmail_sender/mail_sender_program.py
+++ gmail_sender/mail_sender_program.py
@@ -158,3 +158,132 @@ def send_random_message_no_word_meaning_lower_case(email_name, password_for_gmai
         content = content + content1
     
     send_gmail_to_one(email_name, password_for_gmail,to, subject, content)
+
+def send_random_message_no_word_meaning_caps_lock_to_many(email_name, password_for_gmail  ,to, subject ,lettrs):
+    lettrs = int(lettrs)
+    list1 = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z"
+    ]
+    content = ""
+    for i in range(lettrs):
+        content1 = random.choice(list1)
+        content = content + content1
+    
+    send_gmail_to_people_in_list(email_name, password_for_gmail,to, subject, content)
+
+def send_random_message_no_word_meaning_lower_case(email_name, password_for_gmail  ,to, subject ,lettrs):
+    lettrs = int(lettrs)
+    content = ""
+    list1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for i in range(lettrs):
+        content1 = random.choice(list1)
+        content = content + content1
+    
+    send_gmail_to_people_in_list(email_name, password_for_gmail,to, subject, content)
+
+
+def send_gmail_with_meaning(email_name, password_for_gmail,to):
+    listg = [
+        "hello, good morning",
+        "what a good morning",
+        "i had good breakfast",
+        "good morning!",
+        "do you like cornflakes?"
+    ]
+    lista = [
+        "good, afternoon",
+        "good day",
+        "have a good day", 
+        "stay safe", 
+        "today is amazing"
+    ]
+    listr = [
+        "are you having dinner?",
+        "dinner was amazing loved it ",
+        "good night"
+    ]
+    lists = [
+        "hello", 
+        "hallo", 
+        "hi", 
+        "how are you"
+    ]
+    now = datetime.now()
+
+    current_time = now.strftime("%H")
+
+    if current_time == "12" or "12" > current_time and current_time  > "0":
+        content = random.choice(listg)
+
+    if current_time != "12" or "12" < current_time and current_time < "20":
+        content = random.choice(lista)
+    
+    else:
+        content = random.choice(listr)
+    
+    subject = random.choice(lists)
+    send_gmail_to_one(email_name, password_for_gmail, to, subject, content)
+
+def send_gmail_with_meaning_to_many(email_name, password_for_gmail,to):
+    listg = [
+        "hello, good morning",
+        "what a good morning",
+        "i had good breakfast",
+        "good morning!",
+        "do you like cornflakes?"
+    ]
+    lista = [
+        "good, afternoon",
+        "good day",
+        "have a good day", 
+        "stay safe", 
+        "today is amazing"
+    ]
+    listr = [
+        "are you having dinner?",
+        "dinner was amazing loved it ",
+        "good night"
+    ]
+    lists = [
+        "hello", 
+        "hallo", 
+        "hi", 
+        "how are you"
+    ]
+    now = datetime.now()
+
+    current_time = now.strftime("%H")
+
+    if current_time == "12" or "12" > current_time and current_time  > "0":
+        content = random.choice(listg)
+
+    if current_time != "12" or "12" < current_time and current_time < "20":
+        content = random.choice(lista)
+    
+    else:
+        content = random.choice(listr)
+    
+    subject = random.choice(lists)
+    send_gmail_to_people_in_list(email_name, password_for_gmail, to, subject, content)
