82ae6787f29ac86c62de1579b4452f207dbc17b5
Pydevoleper <85053735+Pydevoleper@users.noreply.github.com>
2021-08-13 18:44:57 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     LONG_DESCRIPTION  = f.read()
     
-VERSION = '1.3'
+VERSION = '1.4'
 DESCRIPTION = 'gmail sending lib'
 URL = 'https://pypi.org/project/mail-sending-program/'
 # Setting up
